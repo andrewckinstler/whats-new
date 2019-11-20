cdcbba03e9fd6b193f776d151fab0f32deb7360a
Andrew Kinstler <andrewckinstler@gmail.com>
2019-11-19 19:08:41 -0700

    Initialize NewsContainer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import NewsContainer from '../NewsContainer/NewsContainer'
 
 class App extends Component {
   constructor() {
@@ -13,6 +14,7 @@ class App extends Component {
   render () {
     return (
       <div className="app">
+        <NewsContainer article={local} />
         YOUR CODE GOES HERE!
       </div>
     );
--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import './NewsContainer.css'
+import React, { Component } from 'react';
+import './NewsContainer.css';
+import { longStackSupport } from 'q';
+// import { ReactComponent } from '.svg';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = ({articles}) => {
+  console.log(articles);
+  
+  return (
+    <section className='container'>
+      
+    </section>
+  )
+}
+
 
 export default NewsContainer;
