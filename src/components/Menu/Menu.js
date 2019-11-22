3d02cc9facb7020036c69a6550cfd469116692c8
Andrew Kinstler <andrewckinstler@gmail.com>
2019-11-22 16:48:51 -0700

    Menu tests

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,11 +5,11 @@ const Menu = ({ changeCategory, local, entertainment, health, science, technolog
   return (
     <nav>
       <h1>What's New</h1>
-      <button onClick={() => changeCategory(local)}>Local News</button>
-      <button onClick={() => changeCategory(entertainment)}>Entertainment</button>
-      <button onClick={() => changeCategory(health)}>Health</button>
-      <button onClick={() => changeCategory(science)}>Science</button>
-      <button onClick={() => changeCategory(technology)}>Technology</button>
+      <button className='local' onClick={() => changeCategory(local)}>Local News</button>
+      <button className='entertainment' onClick={() => changeCategory(entertainment)}>Entertainment</button>
+      <button className='health' onClick={() => changeCategory(health)}>Health</button>
+      <button className='science' onClick={() => changeCategory(science)}>Science</button>
+      <button className='tech' onClick={() => changeCategory(technology)}>Technology</button>
     </nav>
   )
 }
