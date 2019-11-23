1fcf1c904d72aeda382342e82636fec1a216adbd
Andrew Kinstler <andrewckinstler@gmail.com>
2019-11-22 17:14:18 -0700

    Add test for state change on button click

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
 
-it('should create a boilerplate of the app', () => {
-  const wrapper = shallow(<App />)
+describe('App', () => {
+  it('should create a boilerplate of the app', () => {
+    const wrapper = shallow( <App /> )
 
-  expect(wrapper).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should update state when changeCategory is called', () => {
+    const wrapper = shallow(<App />)
+
+    wrapper.instance().changeCategory('state');
+    expect(wrapper.state('news')).toEqual('state')
+  })
 })
